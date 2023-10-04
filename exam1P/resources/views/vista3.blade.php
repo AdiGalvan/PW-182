ae8121ae2aa9666fe77cbf0983f6766caf2fba98
AdiGalvan <133288217+AdiGalvan@users.noreply.github.com>
2023-10-04 16:33:19 -0600

    examen p1
    se crearon las vistas con sus rutas y la palntilla

---
+++ exam1P/resources/views/vista3.blade.php
@@ -0,0 +1,10 @@
+
+@extends('layouts.plantilla_examen')
+
+@section('titulo,'Inicio')// si se define de esta forma se cierra automaticamente, no es necsesario cerrar
+
+@section('contenido')
+
+<h1 class="display-1 text-center text-danger" > VISTA3 </h1>
+
+@endsection('contenido')
---
+++ exam1P/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+*/
+
+Route::view( '/', 'inicio')->name('apodoInicio');
+Route::view( '/vista1', 'vista1')->name('apodoVista1');
+Route::view( '/vista2', 'vista2')->name('apodoVista2');
+Route::view( '/vista3', 'vista3')->name('apodoFoVista3');
+
+
+
